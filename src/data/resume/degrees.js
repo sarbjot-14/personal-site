283c687786d6b187b1ca6543a9e83177b61a7ac1
Sarbjot Singh <sarbjot_14@hotmail.com>
2022-01-30 20:36:23 -0800

    first commit

---
+++ src/data/resume/degrees.js
@@ -0,0 +1,10 @@
+const degrees = [
+  {
+    school: 'Simon Fraser University',
+    degree: 'Computer Science',
+    link: 'https://www.sfu.ca/computing.html',
+    year: 2021,
+  },
+];
+
+export default degrees;

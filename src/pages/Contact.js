323039226a2c95e47725b2671088ff979a4bd1db
Sarbjot Singh <38801533+sarbjot-14@users.noreply.github.com>
2022-03-08 20:55:33 -0800

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,7 +17,7 @@ const Contact = () => (
         </div>
       </header>
       <div className="email-at">
-        <p>Feel free to get in touch. You can email me at: </p>
+        <p>Feel free to get in touch, even just to say hi. You can email me at: </p>
         <EmailLink />
       </div>
       <ContactIcons />
